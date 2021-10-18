7b403f9018f8cbcb8317dea96d47c4db704f7bac
Xiushan Lu <xiushan.lu@amlogic.com>
2021-10-18 16:39:41 +0800

    audio: remove useless code. [1/1]
    PD#SWPL-61300
    
    Problem:
    remove useless code.
    
    Solution:
    remove useless code.
    
    Verify:
    locally passed.
    
    Change-Id: I9344c804303f04e6bf072caa1455e0f2e52a04f7
    Signed-off-by: Xiushan Lu <xiushan.lu@amlogic.com>
    Signed-off-by: chunlong.cao <chunlong.cao@amlogic.com>

--- libaudioeffect/TruSurround/tshd_wrapper.h
+++ libaudioeffect/TruSurround/tshd_wrapper.h
@@ -1,12 +1,21 @@
 /*
- *  COPYRIGHT
- *      Copyright 2006 SRS Labs, Inc.  All rights reserved.
+ * Copyright (C) 2018 Amlogic Corporation.
  *
- *      SRS Labs, Inc. may have patents, patent applications, or other
- *      intellectual property rights covering subject matter in this document.
- *      This software may only be used and/or reproduced under license
- *      from SRS Labs, Inc.  Any full or partial reproduction of this
- *      software source code shall retain this notice.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  DESCRIPTION:
+ *      This file implements a special EQ  from Amlogic.
+ *
  */
 
 
